3ae453a538c36049ea42386d18da0b238808c350
slebras@lbl.gov <slebras@Sebastians-MacBook-Pro.local>
2018-12-05 09:56:34 -0800

    fixing bug in genomeset reading

--- lib/slebrasRoary/utils/roary_inputs.py
+++ lib/slebrasRoary/utils/roary_inputs.py
@@ -11,174 +11,184 @@ from installed_clients.DataFileUtilClient import DataFileUtil
 from installed_clients.GenomeFileUtilClient import GenomeFileUtil
 from installed_clients.AssemblyUtilClient import AssemblyUtil
 
+
 def download_gffs(cb_url, scratch, genome_set_ref):
-	"""
-	Args:
-    	cb_url - callback server URL
-    	scratch - scratch work folder
-    	genome_set_ref - reference to genome_set object in workspace
-	Returns the path to the folder containing .gff files
+    """
+    Args:
+    cb_url - callback server URL
+    scratch - scratch work folder
+    genome_set_ref - reference to genome_set object in workspace
+    Returns the path to the folder containing .gff files
 
 
-	we want to first handle a GenomeSet Object "KBaseSearch.GenomeSet" or "KBaseSets.GenomeSet"
-	"""
+    we want to first handle a GenomeSet Object "KBaseSearch.GenomeSet" or "KBaseSets.GenomeSet"
+    """
 
-	# Get our utilities
-	dfu = DataFileUtil(cb_url)
-	au  = AssemblyUtil(cb_url)
-	gfu = GenomeFileUtil(cb_url)
+    # Get our utilities
+    dfu = DataFileUtil(cb_url)
+    au = AssemblyUtil(cb_url)
+    gfu = GenomeFileUtil(cb_url)
 
+    obj_data = dfu.get_objects({'object_refs': [genome_set_ref]})
+    gs_obj = obj_data['data'][0]
+    obj_type = obj_data['info'][2]
 
-	obj_data = dfu.get_objects({'object_refs':[genome_set_ref]})
-	gs_obj = obj_data['data'][0]
-	obj_type = obj_data['info'][2]
+    if 'KBaseSets.GenomeSet' in obj_type:
+        refs = [gsi['ref'] for gsi in gs_obj['items']]
+    elif 'KBaseSearch.GenomeSet' in obj_type:
+        refs = [gse['ref'] for gse in gs_obj['elements'].values()]
+    else:
+        raise TypeError(
+            'provided input must of type KBaseSets.GenomeSet or KBaseSearch.GenomeSet not ' +
+            str *
+            (obj_type))
 
-	if 'KBaseSets.GenomeSet' in obj_type:
-		refs = [gsi['ref'] for gsi in gs_obj['items']]
-	elif 'KBaseSearch.GenomeSet' in obj_type:
-		refs = [gse['ref'] for gse in gs_obj['elements']]
-	else:
-		raise TypeError('provided input must of type KBaseSets.GenomeSet or KBaseSearch.GenomeSet not '+ str*(obj_type))
+    # name the output directory
+    temp_dir = scratch + '/temp'
+    final_dir = scratch + '/gff'
 
-	# name the output directory
-	temp_dir = scratch + '/temp'
-	final_dir = scratch + '/gff'
+    os.mkdir(final_dir)
+    os.mkdir(temp_dir)
 
-	os.mkdir(final_dir)
-	os.mkdir(temp_dir)
+    # write file that will help us cat the gff and fasta files
+    cat_path = scratch + '/fast_cat.txt'
 
-	# write file that will help us cat the gff and fasta files
-	cat_path = scratch + '/fast_cat.txt'
+    cat_file = open(cat_path, 'w')
+    cat_file.write("##FASTA")
+    cat_file.close()
 
-	cat_file = open(cat_path,'w')	
-	cat_file.write("##FASTA")
-	cat_file.close()
+    path_to_ref_and_ID_pos_dict = {}
+    for ref in refs:
+        gen_obj = dfu.get_objects({'object_refs': [ref]})['data'][0]
 
-	path_to_ref_and_ID_pos_dict = {}
-	for ref in refs:
-		gen_obj = dfu.get_objects({'object_refs':[ref]})['data'][0]
+        # figure out if genome is of acceptable type.
+        # NO Eukaryotes, NO Fungi,
+        # yes bacateria, yes archaea, yes(?) virus
+        if gen_obj['domain'] not in ['Bacteria', 'Archaea']:
+            raise TypeError(
+                'Provided Genomes are not labeled as Bacteria or Archaea. Roary is only equipped to handle Archaea or Bacteria')
 
-		# figure out if genome is of acceptable type.
-		# NO Eukaryotes, NO Fungi, 
-		# yes bacateria, yes archaea, yes(?) virus
-		if gen_obj['domain'] not in ['Bacteria','Archaea']:
-			raise TypeError('Provided Genomes are not labeled as Bacteria or Archaea. Roary is only equipped to handle Archaea or Bacteria')
+        fasta_path = temp_dir + "/" + gen_obj['id'] + ".fa"
+        gff_file = gfu.genome_to_gff({'genome_ref': ref, 'target_dir': temp_dir})
+        fasta_file = au.get_assembly_as_fasta(
+            {'ref': gen_obj['assembly_ref'], 'filename': fasta_path})
 
-		fasta_path = temp_dir + "/" + gen_obj['id'] + ".fa"
-		gff_file = gfu.genome_to_gff({'genome_ref':ref, 'target_dir':temp_dir})
-		fasta_file = au.get_assembly_as_fasta({'ref':gen_obj['assembly_ref'], 'filename': fasta_path})
+        # need to figure out if FASTA is already in gff file
+        # not sure if we need to do this step.
+        gff_file_path, ID_to_pos, contains_fasta = filter_gff(gff_file['path'])
 
-		# need to figure out if FASTA is already in gff file
-		# not sure if we need to do this step. 
-		gff_file_path, ID_to_pos, contains_fasta = filter_gff(gff_file['path'])
+        new_file_path = final_dir + "/" + gen_obj['id'] + ".gff"
 
-		new_file_path = final_dir + "/" + gen_obj['id'] + ".gff"
+        if contains_fasta:
+            args = ['mv', gff_file_path, new_file_path]
+            subprocess.call(args)
+        else:
+            # next we make a new 'gff' file that contains both the gff and fasta information
+            args = ['cat', fasta_file['path'], cat_path, gff_file_path, '>', new_file_path]
+            subprocess.call(args)
 
-		if contains_fasta:
-			args = ['mv',gff_file_path,new_file_path]
-			subprocess.call(args)
-		else:
-			# next we make a new 'gff' file that contains both the gff and fasta information
-			args = ['cat',fasta_file['path'], cat_path, gff_file_path, '>', new_file_path]
-			subprocess.call(args)
+        path_to_ref_and_ID_pos_dict[new_file_path] = (ref, ID_to_pos)
 
-		path_to_ref_and_ID_pos_dict[new_file_path] = (ref, ID_to_pos)
+    return final_dir, path_to_ref_and_ID_pos_dict
 
-	return final_dir, path_to_ref_and_ID_pos_dict
 
 def make_some_trash(gff_file):
-	f = open(gff_file)
-	output = []
-	IDs = set([])
-	cds_to_pos = {}
-	gene_pos = 0
-	for l in f:
-		if '##FASTA' in l:
-			output.append(l)
-			output += [j for j in f]
-			break
-		elif l[:2] == '##':
-			output.append(l)
-			continue
-		ID = l.split(';')[0].split('=')[-1]
-		feat_type = l.split()[2]
-		if feat_type == 'CDS':
-			cds_to_pos[ID] = gene_pos
-			gene_pos += 1
-		output.append(l)
-	f.close()
+    f = open(gff_file)
+    output = []
+    IDs = set([])
+    cds_to_pos = {}
+    gene_pos = 0
+    for l in f:
+        if '##FASTA' in l:
+            output.append(l)
+            output += [j for j in f]
+            break
+        elif l[:2] == '##':
+            output.append(l)
+            continue
+        ID = l.split(';')[0].split('=')[-1]
+        feat_type = l.split()[2]
+        if feat_type == 'CDS':
+            cds_to_pos[ID] = gene_pos
+            gene_pos += 1
+        output.append(l)
+    f.close()
 
-	# write output to file
-	f = open(gff_file, 'w')
-	for l in output: f.write(l)
-	f.close()
+    # write output to file
+    f = open(gff_file, 'w')
+    for l in output:
+        f.write(l)
+    f.close()
 
-	return gff_file, cds_to_pos, True
+    return gff_file, cds_to_pos, True
+
 
 def filter_gff(gff_file):
-	# if there is a duplicate ID's toss one of them out (if it is programmed frameshift toss one)
-	# either the smaller or the second one.
-	# for feature pos, use the order of the gff 'gene' items. create a mapping from ID to order for feature pos
-	f = open(gff_file)
-	output = []
-	IDs = set([])
-	# definitely a better way of doing this
+    # if there is a duplicate ID's toss one of them out (if it is programmed frameshift toss one)
+    # either the smaller or the second one.
+    # for feature pos, use the order of the gff 'gene' items. create a mapping
+    # from ID to order for feature pos
+    f = open(gff_file)
+    output = []
+    IDs = set([])
+    # definitely a better way of doing this
 
-	# ideally we would do the feature mapping o th genes, but there are nt always 'gene' features in the gff files
-	# so to get around this we base the gene position in the gff off the CDS position because they are most often
-	# consecutively ordered and paired. For the organisms that Roary is supposed to service, there should only be
-	# a one to one pairing of 'gene' to 'CDS' 
+    # ideally we would do the feature mapping o th genes, but there are nt always 'gene' features in the gff files
+    # so to get around this we base the gene position in the gff off the CDS position because they are most often
+    # consecutively ordered and paired. For the organisms that Roary is supposed to service, there should only be
+    # a one to one pairing of 'gene' to 'CDS'
 
-	# gene_to_pos = {}
-	# cds_to_gene = {}
+    # gene_to_pos = {}
+    # cds_to_gene = {}
 
-	cds_to_pos = {}
-	gene_pos = 0
-	contains_fasta = False
-	for l in f:
-		if "##FASTA" in l:
-			# add FASTA sequence to output if it exists and then exit
-			contains_fasta = True
-			output.append(l)
-			output += [j for j in f]
-			break
-		# if we find a comment add it to output
-		if l[0:2] == '##':
-			output.append(l)
-			continue
+    cds_to_pos = {}
+    gene_pos = 0
+    contains_fasta = False
+    for l in f:
+        if "##FASTA" in l:
+            # add FASTA sequence to output if it exists and then exit
+            contains_fasta = True
+            output.append(l)
+            output += [j for j in f]
+            break
+        # if we find a comment add it to output
+        if l[0:2] == '##':
+            output.append(l)
+            continue
 
-		# get ID of feature
-		ID = l.split(';')[0].split('=')[-1]
-		# determine type of feature
-		feat_type = l.split()[2]
-		# if feat_type == 'gene':
-		# 	gene_to_pos[ID] = gene_pos
-		# 	gene_pos += 1
-		if feat_type == 'CDS':
-			cds_to_pos[ID] = gene_pos
-			gene_pos += 1
-			# # find parent
-			# parent_split = l.split('Parent=')
-			# if len(parent_split) == 2:
-			# 	parent_gene_ID = parent_split[1].split(';')[0]
-			# 	cds_to_gene[ID] = parent_gene_ID
-			# elif len(parent_split) == 1:
-			# 	# there is no parent to this feature.
+        # get ID of feature
+        ID = l.split(';')[0].split('=')[-1]
+        # determine type of feature
+        feat_type = l.split()[2]
+        # if feat_type == 'gene':
+        # 	gene_to_pos[ID] = gene_pos
+        # 	gene_pos += 1
+        if feat_type == 'CDS':
+            cds_to_pos[ID] = gene_pos
+            gene_pos += 1
+            # # find parent
+            # parent_split = l.split('Parent=')
+            # if len(parent_split) == 2:
+            # 	parent_gene_ID = parent_split[1].split(';')[0]
+            # 	cds_to_gene[ID] = parent_gene_ID
+            # elif len(parent_split) == 1:
+            # 	# there is no parent to this feature.
 
-		ids_len = len(IDs)
-		IDs.add(ID)
-		if len(IDs) == ids_len:
-			# we found a duplicate and its the second one. get rid of it
-			continue
-		else:
-			# make sure that the feature we get is a 'CDS' object
-			if feat_type == 'CDS':
-				output.append(l)
-	f.close()
+        ids_len = len(IDs)
+        IDs.add(ID)
+        if len(IDs) == ids_len:
+            # we found a duplicate and its the second one. get rid of it
+            continue
+        else:
+            # make sure that the feature we get is a 'CDS' object
+            if feat_type == 'CDS':
+                output.append(l)
+    f.close()
 
-	# write output to file
-	f = open(gff_file, 'w')
-	for l in output: f.write(l)
-	f.close()
+    # write output to file
+    f = open(gff_file, 'w')
+    for l in output:
+        f.write(l)
+    f.close()
 
-	return gff_file, cds_to_pos, contains_fasta
+    return gff_file, cds_to_pos, contains_fasta
